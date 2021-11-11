7967bcf607a3f3996b79b020b4e812649e7c50f0
alucard <devjrteste@gmail.com>
2021-11-10 21:11:03 -0300

    chore: criando logica de button ativo ou nao

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,11 +1,18 @@
 import { FaGithub } from 'react-icons/fa';
 import styles from './styles.module.scss';
 
+const isUserLoggedIn = false;
+
 export function SignInButton() {
-    return (
+    return isUserLoggedIn ? (
+        <button className={styles.signInButton} type="button">
+            <FaGithub color="#04d361" />
+            Sign in with Github
+        </button>
+    ) : (
         <button className={styles.signInButton} type="button">
             <FaGithub color="#AC5532" />
             Sign in with Github
         </button>
-    );
+    )
 }
