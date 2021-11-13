db387d76ae9eb6f7dec6ac9878ccb2d4e9fb6120
alucard <devjrteste@gmail.com>
2021-11-12 22:54:44 -0300

    chore: component home finalizado

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,13 +1,33 @@
 import Head from 'next/head';
-
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.new</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <img src="/images/cristal.png" alt="cristal" />
+
+          <span>Hey, welcome</span>
+
+          <h1>News about medial
+            project using <span>JavaScript.</span>
+          </h1>
+
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.99 month</span>
+          </p>
+
+        </section>
+
+        <img src="/images/avatar.png" alt="viking" />
+
+      </main>
     </>
   );
 }
