36c26bdb6e808a8272bf619dbc0f7e781646c665
alucard <devjrteste@gmail.com>
2021-11-24 20:38:18 -0300

    chore: aplicando SSG

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { stripe } from '../services/stripe'
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
@@ -44,7 +44,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1JyPT8F9rYny3aLO6teoV9m4', {
 
@@ -62,7 +62,9 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
     props: {
       product,
-    }
+    },
+
+    revalidate: 60 * 60 * 24, //24 hours
 
   }
 
