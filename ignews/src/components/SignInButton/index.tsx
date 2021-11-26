4b9fd40a96f4223ab88dcccecf8f05c546fb94e8
alucard <devjrteste@gmail.com>
2021-11-26 02:37:15 -0300

    chore: Autenticação com Next Auth

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,19 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
-
-
-const isUserLoggedIn = true;
+import { signIn, useSession } from 'next-auth/client';
 
 export function SignInButton() {
-    return isUserLoggedIn ? (
-        <button className={styles.signInButton} type="button">
+    const [session] = useSession()
+
+    console.log(session)
+
+    return session ? (
+
+        <button className={styles.signInButton}
+            type="button">
             <FaGithub color="#04d361" />
             Natália Araújo
-            <FiX color="#737380" className={styles.closeIcon} />
+            <FiX color="#737380"
+                className={styles.closeIcon} />
         </button>
+
     ) : (
-        <button className={styles.signInButton} type="button">
+        <button type="button">
+            className={styles.signInButton}
+            onClick={() => signIn('github')}
+
             <FaGithub color="#AC5532" />
             Sign in with Github
         </button>
