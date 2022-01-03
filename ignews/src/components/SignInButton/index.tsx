3005b05582cfcb6a3b1083bcf3deabaedb2b37af
nataliaaraujo0 <devjrteste@gmail.com>
2022-01-03 01:31:05 -0300

    fix: autenticacao com next auth

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,23 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
-import { signIn, useSession } from 'next-auth/client';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 export function SignInButton() {
-    const [session] = useSession()
+  const [session] = useSession()
 
-    console.log(session)
+  console.log(session)
 
-    return session ? (
-        <button className={styles.signInButton} type="button">
-            <FaGithub color="#04d361" />
-            Natália Araújo
-            <FiX color="#737380" className={styles.closeIcon} />
-        </button>
-    ) : (
-        <button type="button" className={styles.signInButton} onClick={() => signIn('github')}>
-            <FaGithub color="#AC5532" />
-            Sign in with Github
-        </button>
-    )
-}
+  return session ? (
+    <button type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color="#04d361" />
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}>
+      <FaGithub color="#AC5532" />
+      Sign in with Github
+    </button>
+  )
+} 
