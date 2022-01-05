9b304ae717d3118f4f34c505da222360f7d3ee84
nataliaaraujo0 <devjrteste@gmail.com>
2022-01-05 02:36:37 -0300

    chore:Redirecionando para o Stripe

--- ignews/src/pages/api/subscribe.ts
+++ ignews/src/pages/api/subscribe.ts
@@ -14,7 +14,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       customer: stripeCustomer.id,
       payment_method_types: ["card"],
       billing_address_collection: "required",
-      line_items: [{ price: " price_1JyPT8F9rYny3aLO6teoV9m4", quantity: 1 }],
+      line_items: [
+        {
+          price: "price_1JyPT8F9rYny3aLO6teoV9m4",
+          quantity: 1,
+        },
+      ],
       mode: "subscription",
       allow_promotion_codes: true,
       success_url: process.env.STRIPE_SUCCESS_URL,
